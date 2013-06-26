e3f27683c53361db7359293d2fc6c9af3df00850
Alex Pentland <apentlander@gmail.com>
2013-06-26 15:53:26 -0400

    Separated graphing from the main program
    The central graphing widget was moved to a separate class to allow the
    main window to be used in different projects in the future. The creation
    of the graph and the buttons is now under the control of the graphing
    widget class

---
+++ gui/graphingwidget.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 16:27:54 2013
+
+@author: pentla
+"""
+import PyQt4.QtGui as qt
+import daysimdata as dd
+from PyQt4.QtCore import Signal
+
+class GraphingWidget(qt.QWidget):
+    """A graphing widget that contains a graph and a button box with radio 
+    buttons for controlling the plots shown on the graph"""
+    
+    def __init__(self, parent=None):
+        """Creates a grapging widget witha playholder graph and button box"""
+        super(GraphingWidget, self).__init__(parent)
+        self.data = dd.DaysimeterData()
+        self.buttons = ButtonBox(self)
+        self.plot = self.data.get_plot()
+        
+        self.layout = qt.QVBoxLayout()
+        self.layout.addWidget(self.plot)
+        self.layout.addWidget(self.buttons)
+        self.setLayout(self.layout)
+        
+    def set_data(self, timestamps, data):
+        """Sets the data from the daysimeter and creates the plot and buttons
+        
+        timestamps - An ndarray of date times
+        data - A recarray of the data values (e.g. Lux, CS, etc.)
+        
+        """
+        # Creates a new daysimeter data object, sets its data, and creates the
+        # new plots
+        self.data = dd.DaysimeterData()
+        self.data.set_values(timestamps, data)
+        self.data.make_plots()
+        # Deletes the current plot in tht widget, sets it to the plot in the
+        # new data, and adds it to this widget
+        self.plot.deleteLater()
+        self.plot = self.data.get_plot()
+        self.layout.addWidget(self.plot)
+        
+        # Deletes the current set of buttons, creates a new set, and adds it to
+        # this widget
+        self.buttons.deleteLater()
+        self.buttons = ButtonBox(self).make_buttons(self.data.get_data_names())
+        self.layout.addWidget(self.buttons)
+        # Shows the set of plots with their corresponding buttons checked
+        self.buttons.buttonsChecked.connect(self.show_plots)
+        
+    def show_plots(self, names):
+        """Only shows the subplots listed in names
+        
+        names - list of strings that represents the names of the graph values
+                to show
+                
+        """
+        self.data.show_plots(names)
+        self.plot.draw()
+        
+class ButtonBox(qt.QGroupBox):
+    """A button box that contains radio buttons created based on the data that
+    will emit the list of buttons currently checked when any is pressed"""
+    
+    buttonsChecked = Signal(list)
+    def __init__(self, parent=None):
+        """Initializes the buttons box with the title 'Graph Options'"""
+        super(ButtonBox, self).__init__(parent)
+        self._button_group = qt.QButtonGroup()
+        self.setTitle("Graph Options")
+        
+    def make_buttons(self, names=['Lux', 'CLA', 'CS', 'Activity']):
+        """Creates buttons based on the names given
+        
+        names - A list of strings representing the names of the buttons that
+                should be made
+        
+        """
+        self._button_group = qt.QButtonGroup()
+        self._button_group.setExclusive(False)
+        vbox = qt.QVBoxLayout()
+        # Creates a button for each name in the list given and adds that button
+        # to the button group (container for the objects) and the button box 
+        # (container for the buttons in the GUI)
+        for name in names:
+            button = qt.QRadioButton(name, self)
+            self._button_group.addButton(button)
+            vbox.addWidget(button)
+        self.setLayout(vbox)
+        self._button_group.buttonClicked.connect(self.get_buttons_pressed)
+        return self
+        
+        
+    def get_buttons_pressed(self):
+        """Emits a list of button names that are currently checked"""
+        button_names = []
+        for button in self._button_group.buttons():
+            if button.isChecked():
+                button_names.append(str(button.text()))
+        self.buttonsChecked.emit(button_names)
--- gui/test.py
+++ gui/test.py
@@ -9,7 +9,7 @@ import sys
 import PyQt4.QtGui as qt
 import numpy as np
 import datetime as dt
-import daysimdata as dd
+import graphingwidget as gw
 
 QT_APP = qt.QApplication(sys.argv) 
  
@@ -18,49 +18,26 @@ class LayoutExample(qt.QMainWindow):
         qt.QMainWindow.__init__(self)
         self.setWindowTitle('Daysimeter Download Client')
         self.setMinimumSize(600, 400)
-        self.main_widget = qt.QWidget(self)
-        
-        layout = qt.QVBoxLayout()
-        form_layout = qt.QFormLayout()
-        form_layout.addWidget(qt.QLabel('hi', self))
-        
-        self.data = dd.DaysimeterData()
-        self.plot = self.data.get_plot()
-        self.button_box = qt.QGroupBox('Graph Options')
-        
-        layout.addWidget(self.plot)
-        layout.addWidget(self.button_box)
-        self.main_widget.setLayout(layout)        
-        
+        self.main_widget = gw.GraphingWidget(self)
         self.setCentralWidget(self.main_widget)
-        
-        self.create_actions()
+        #self.create_actions()
         self.create_menus()
         
     def create_menus(self):
         file_menu = self.menuBar().addMenu("&File")
-        file_menu.addAction(self.open_act)
+        file_actions = self.create_menu_actions()
+        file_menu.addAction(file_actions)
         
-    def create_actions(self):
-        self.open_act = qt.QAction("&Open...", self)
-        self.open_act.setStatusTip = "Open a processed daysimeter file",
-        self.open_act.triggered.connect(self.open_file)
+    def create_menu_actions(self):
+        open_act = qt.QAction("&Open...", self)
+        open_act.setStatusTip = "Open a processed daysimeter file",
+        open_act.triggered.connect(self.open_file)
         
-    def make_buttons(self, names):
-        self.button_group = qt.QButtonGroup()
-        self.button_group.setExclusive(False)
-        vbox = qt.QVBoxLayout()
-        button_dict = {}
-        for name in names:
-            button_dict[name] = qt.QRadioButton(name, self.button_box)
-            self.button_group.addButton(button_dict[name])
-            vbox.addWidget(button_dict[name])
-        self.button_box.setLayout(vbox)
-        self.button_group.buttonClicked.connect(self.make_plot)
+        quit_act = qt.QAction("&Quit", self)
         
-    def make_plot(self):
-        self.data.show_plots(self.button_group)
-        self.plot.draw()
+        return open_act
+        
+    def create_action(self, name, status_tip,  function)
         
     def open_file(self):
         file_name = str(qt.QFileDialog.getOpenFileName(self))
@@ -76,8 +53,7 @@ class LayoutExample(qt.QMainWindow):
         timestamps = [dt.datetime.strptime(datetime_str[x], "%m/%d/%Y %H:%M:%S")
                       for x in range(len(datetime_str))]
         daysim_values = self.remove_datetime_fields(daysim_values)
-        self.data.set_values(timestamps, daysim_values)
-        self.make_buttons(self.data.get_data_names())
+        self.main_widget.set_data(timestamps, daysim_values)
         
     def remove_datetime_fields(self, data_array):
         names = list(data_array.dtype.names)
