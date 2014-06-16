a16545222b67264a20df1dbef40d6507c759b9ea
pentla <apentlander@gmail.com>
2014-06-16 16:44:40 -0400

    Migrated the csv function from QThread to QObject

--- src/downloadmake.py
+++ src/downloadmake.py
@@ -42,7 +42,7 @@ class DownloadMake(QtGui.QWidget):
     def __init__(self, offset, parent=None, args=None):
         super(DownloadMake, self).__init__(parent)
         dl_skip_shortcut = QtGui.QShortcut(QtGui.QKeySequence('SHIFT+CTRL+G'),\
-        self, self.skip_prog, self.skip_prog, QtCore.Qt.WidgetShortcut)
+            self, self.skip_prog, self.skip_prog, QtCore.Qt.WidgetShortcut)
         if not args:
             path = find_daysimeter()
             if path:
@@ -50,20 +50,17 @@ class DownloadMake(QtGui.QWidget):
                 self.data_filename = os.path.join(path, \
                                                   constants_.DATA_FILENAME)
                 self.download_make = True
+                self.time_offset_index = offset
             else:
                 sys.exit(1)
         else:
             self.log_filename = args[0]
             self.data_filename = args[1]
             self.download_make = False
-        self.time_offset_index = offset
-    
-        
-        
+            self.time_offset_index = offset
         
         self.daysim_log = logging.getLogger('daysim_log')
         self.err_log = logging.getLogger('err_log')
-        
         self.initUI()
         
         
@@ -140,7 +137,7 @@ class DownloadMake(QtGui.QWidget):
             self.status_bar.showMessage('')
             self.filename = str(QtGui.QFileDialog.getSaveFileName(self, \
                 ('Save File'), default_name, ('CDF Files (*.cdf);; CSV Files (*.csv)')))
-            if not str(self.filename) == '':
+            if str(self.filename) != '':
                 parts = self.filename.split('/')
                 del(parts[-1])
                 savepath = ''
@@ -190,10 +187,13 @@ class DownloadMake(QtGui.QWidget):
         else:
             self.daysim_log.info('downloadmake.py class DownloadMake func make: Preparing CSV file')
             self.status_bar.showMessage('Writing CSV File...')
+            self.maker_thread = QtCore.QThread()
             self.maker = MakeCSV(self, data, self.filename)
-            self.connect(self.maker, QtCore.SIGNAL('update'), \
-                         self.update_progress)
-            self.maker.start()
+            self.maker.moveToThread(self.maker_thread)
+            self.maker.update.connect(self.update_progress)
+            self.maker.finished.connect(self.maker_thread.quit)
+            self.maker_thread.started.connect(self.maker.run)
+            self.maker_thread.start()
             
     def make_cdf(self, info):
         """ PURPOSE: Makes a CDF file. """
@@ -1102,10 +1102,10 @@ class MakeCDF(QtCore.QThread):
             cdf_fp.attrs['redCalibration'] = data[0][2][1]
             cdf_fp.attrs['greenCalibration'] = data[0][2][2]
             cdf_fp.attrs['blueCalibration'] = data[0][2][3]
-    #########The following global attributes either do not exist yet, or
-    #########have no available source to pull the information from
-    #        cdf_fp.attrs['uvCalibration'] = 
-    #        cdf_fp.attrs['illuminanceCalibration'] =
+        #########The following global attributes either do not exist yet, or
+        #########have no available source to pull the information from
+        #        cdf_fp.attrs['uvCalibration'] = 
+        #        cdf_fp.attrs['illuminanceCalibration'] =
             cdf_fp.attrs['subjectID'] = sub_info[0]
             cdf_fp.attrs['subjectSex'] = sub_info[1]
             cdf_fp.attrs['subjectDateOfBirth'] = sub_info[2]
@@ -1114,8 +1114,8 @@ class MakeCDF(QtCore.QThread):
             
             #Set variables
             cdf_fp['time'] = data[1][0]
-#            cdf_fp.new('matTime', type=pycdf.const.CDF_REAL8)
-#            cdf_fp['matTime'] = data[1][1]
+        #            cdf_fp.new('matTime', type=pycdf.const.CDF_REAL8)
+        #            cdf_fp['matTime'] = data[1][1]
             cdf_fp.new('timeOffset', get_local_offset_s(), \
             pycdf.const.CDF_INT4, False)
             cdf_fp['logicalArray'] = self.logical_arr
@@ -1127,14 +1127,14 @@ class MakeCDF(QtCore.QThread):
             cdf_fp['CS'] = data[1][7]
             cdf_fp['activity'] = data[1][8]
             
-    #        cdf_fp['xAcceleration'] =
-    #        cdf_fp['yAcceleration'] =
-    #        cdf_fp['zAcceleration'] =
-    #        cdf_fp['uvIndex'] =
-    #        cdf_fp['temperature'] =
-    #        cdf_fp['longitude'] =
-    #        cdf_fp['latitude'] =
-    #        cdf_fp['event'] =
+        #        cdf_fp['xAcceleration'] =
+        #        cdf_fp['yAcceleration'] =
+        #        cdf_fp['zAcceleration'] =
+        #        cdf_fp['uvIndex'] =
+        #        cdf_fp['temperature'] =
+        #        cdf_fp['longitude'] =
+        #        cdf_fp['latitude'] =
+        #        cdf_fp['event'] =
             
             #Set variable attributes for time
             cdf_fp['time'].attrs['description'] = 'UTC in CDF Epoch format,' + \
@@ -1145,12 +1145,12 @@ class MakeCDF(QtCore.QThread):
             cdf_fp['time'].attrs['otherAttributes'] = ''
             
             #Set variable attributes for matTime
-#            cdf_fp['matTime'].attrs['description'] = 'UTC in MATLAB serial' + \
-#            ' date format, days since 1-Jan-0000'
-#            cdf_fp['matTime'].attrs['unitPrefix'] = ''
-#            cdf_fp['matTime'].attrs['baseUnit'] = 'days'
-#            cdf_fp['matTime'].attrs['unitType'] = 'nonSI'
-#            cdf_fp['matTime'].attrs['otherAttributes'] = ''
+        #            cdf_fp['matTime'].attrs['description'] = 'UTC in MATLAB serial' + \
+        #            ' date format, days since 1-Jan-0000'
+        #            cdf_fp['matTime'].attrs['unitPrefix'] = ''
+        #            cdf_fp['matTime'].attrs['baseUnit'] = 'days'
+        #            cdf_fp['matTime'].attrs['unitType'] = 'nonSI'
+        #            cdf_fp['matTime'].attrs['otherAttributes'] = ''
             
             #Set variable attributes for timeOffset
             cdf_fp['timeOffset'].attrs['description'] = 'Localized offset ' + \
@@ -1218,66 +1218,66 @@ class MakeCDF(QtCore.QThread):
             cdf_fp['activity'].attrs['otherAttributes'] = 'method'
             
             #Set variable attributes for xAcceleration
-    #        cdf_fp['xAcceleration'].attrs['description'] = 'Acceleration ' + \
-    #        'in the x-axis relative to the accelerometer'
-    #        cdf_fp['xAcceleration'].attrs['unitPrefix'] = ''
-    #        cdf_fp['xAcceleration'].attrs['baseUnit'] = 'm/s^2'
-    #        cdf_fp['xAcceleration'].attrs['unitType'] = 'derivedSI'
-    #        cdf_fp['xAcceleration'].attrs['otherAttributes'] = ''
+        #        cdf_fp['xAcceleration'].attrs['description'] = 'Acceleration ' + \
+        #        'in the x-axis relative to the accelerometer'
+        #        cdf_fp['xAcceleration'].attrs['unitPrefix'] = ''
+        #        cdf_fp['xAcceleration'].attrs['baseUnit'] = 'm/s^2'
+        #        cdf_fp['xAcceleration'].attrs['unitType'] = 'derivedSI'
+        #        cdf_fp['xAcceleration'].attrs['otherAttributes'] = ''
             
             #Set variable attributes for yAcceleration
-    #        cdf_fp['yAcceleration'].attrs['description'] = 'Acceleration ' + \ 
-    #        'in the y-axis relative to the accelerometer'
-    #        cdf_fp['yAcceleration'].attrs['unitPrefix'] = ''
-    #        cdf_fp['yAcceleration'].attrs['baseUnit'] = 'm/s^2'
-    #        cdf_fp['yAcceleration'].attrs['unitType'] = 'derivedSI'
-    #        cdf_fp['yAcceleration'].attrs['otherAttributes'] = ''
+        #        cdf_fp['yAcceleration'].attrs['description'] = 'Acceleration ' + \ 
+        #        'in the y-axis relative to the accelerometer'
+        #        cdf_fp['yAcceleration'].attrs['unitPrefix'] = ''
+        #        cdf_fp['yAcceleration'].attrs['baseUnit'] = 'm/s^2'
+        #        cdf_fp['yAcceleration'].attrs['unitType'] = 'derivedSI'
+        #        cdf_fp['yAcceleration'].attrs['otherAttributes'] = ''
             
             #Set variable attributes for zAcceleration
-    #        cdf_fp['zAcceleration'].attrs['description'] = 'Acceleration ' + \
-    #        'in the z-axis relative to the accelerometer'
-    #        cdf_fp['zAcceleration'].attrs['unitPrefix'] = ''
-    #        cdf_fp['zAcceleration'].attrs['baseUnit'] = 'm/s^2'
-    #        cdf_fp['zAcceleration'].attrs['unitType'] = 'derivedSI'
-    #        cdf_fp['zAcceleration'].attrs['otherAttributes'] = ''
-            
-            #Set variable attributes for uvIndex
-    #        cdf_fp['uvIndex'].attrs['description'] = 'Ultraviolet index'
-    #        cdf_fp['uvIndex'].attrs['unitPrefix'] = ''
-    #        cdf_fp['uvIndex'].attrs['baseUnit'] = 'uvIndex'
-    #        cdf_fp['uvIndex'].attrs['unitType'] = 'nonSI'
-    #        cdf_fp['uvIndex'].attrs['otherAttributes'] = ''
-            
-            #Set variable attributes for temperature
-    #        cdf_fp['temperature'].attrs['description'] = 'Ambient air ' + \
-    #        'temperature in degrees Kelvin'
-    #        cdf_fp['temperature'].attrs['unitPrefix'] = ''
-    #        cdf_fp['temperature'].attrs['baseUnit'] = 'K'
-    #        cdf_fp['temperature'].attrs['unitType'] = 'baseSI'
-    #        cdf_fp['temperature'].attrs['otherAttributes'] = ''
-            
-            #Set variable attributes for longitude
-    #        cdf_fp['longitude'].attrs['description'] = 'Longitude in ' + \
-    #        'decimal degrees'
-    #        cdf_fp['longitude'].attrs['unitPrefix'] = ''
-    #        cdf_fp['longitude'].attrs['baseUnit'] = 'deg'
-    #        cdf_fp['longitude'].attrs['unitType'] = 'nonSI'
-    #        cdf_fp['longitude'].attrs['otherAttributes'] = ''
-            
-            #Set variable attributes for latitude
-    #        cdf_fp['latitude'].attrs['description'] = 'Latitude in ' + \
-    #        'decimal degrees'
-    #        cdf_fp['latitude'].attrs['unitPrefix'] = ''
-    #        cdf_fp['latitude'].attrs['baseUnit'] = 'deg'
-    #        cdf_fp['latitude'].attrs['unitType'] = 'nonSI'
-    #        cdf_fp['latitude'].attrs['otherAttributes'] = ''
-            
-            #Set variable attributes for event
-    #        cdf_fp['event'].attrs['description'] = 'Event marker'
-    #        cdf_fp['event'].attrs['unitPrefix'] = ''
-    #        cdf_fp['event'].attrs['baseUnit'] = ''
-    #        cdf_fp['event'].attrs['unitType'] = ''
-    #        cdf_fp['event'].attrs['otherAttributes'] = 'event code definition'
+        #        cdf_fp['zAcceleration'].attrs['description'] = 'Acceleration ' + \
+        #        'in the z-axis relative to the accelerometer'
+        #        cdf_fp['zAcceleration'].attrs['unitPrefix'] = ''
+        #        cdf_fp['zAcceleration'].attrs['baseUnit'] = 'm/s^2'
+        #        cdf_fp['zAcceleration'].attrs['unitType'] = 'derivedSI'
+        #        cdf_fp['zAcceleration'].attrs['otherAttributes'] = ''
+                
+                #Set variable attributes for uvIndex
+        #        cdf_fp['uvIndex'].attrs['description'] = 'Ultraviolet index'
+        #        cdf_fp['uvIndex'].attrs['unitPrefix'] = ''
+        #        cdf_fp['uvIndex'].attrs['baseUnit'] = 'uvIndex'
+        #        cdf_fp['uvIndex'].attrs['unitType'] = 'nonSI'
+        #        cdf_fp['uvIndex'].attrs['otherAttributes'] = ''
+                
+                #Set variable attributes for temperature
+        #        cdf_fp['temperature'].attrs['description'] = 'Ambient air ' + \
+        #        'temperature in degrees Kelvin'
+        #        cdf_fp['temperature'].attrs['unitPrefix'] = ''
+        #        cdf_fp['temperature'].attrs['baseUnit'] = 'K'
+        #        cdf_fp['temperature'].attrs['unitType'] = 'baseSI'
+        #        cdf_fp['temperature'].attrs['otherAttributes'] = ''
+                
+                #Set variable attributes for longitude
+        #        cdf_fp['longitude'].attrs['description'] = 'Longitude in ' + \
+        #        'decimal degrees'
+        #        cdf_fp['longitude'].attrs['unitPrefix'] = ''
+        #        cdf_fp['longitude'].attrs['baseUnit'] = 'deg'
+        #        cdf_fp['longitude'].attrs['unitType'] = 'nonSI'
+        #        cdf_fp['longitude'].attrs['otherAttributes'] = ''
+                
+                #Set variable attributes for latitude
+        #        cdf_fp['latitude'].attrs['description'] = 'Latitude in ' + \
+        #        'decimal degrees'
+        #        cdf_fp['latitude'].attrs['unitPrefix'] = ''
+        #        cdf_fp['latitude'].attrs['baseUnit'] = 'deg'
+        #        cdf_fp['latitude'].attrs['unitType'] = 'nonSI'
+        #        cdf_fp['latitude'].attrs['otherAttributes'] = ''
+                
+                #Set variable attributes for event
+        #        cdf_fp['event'].attrs['description'] = 'Event marker'
+        #        cdf_fp['event'].attrs['unitPrefix'] = ''
+        #        cdf_fp['event'].attrs['baseUnit'] = ''
+        #        cdf_fp['event'].attrs['unitType'] = ''
+        #        cdf_fp['event'].attrs['otherAttributes'] = 'event code definition'
             
         #Set download flag to true (0)
         self.daysim_log.info('All data written to CDF file')
@@ -1288,10 +1288,13 @@ class MakeCDF(QtCore.QThread):
         
         self.emit(QtCore.SIGNAL('update'))
 
-class MakeCSV(QtCore.QThread):
+class MakeCSV(QtCore.QObject):
+
+    finished = QtCore.pyqtSignal()
+    update = QtCore.pyqtSignal()
     
     def __init__(self, parent, data, filename):
-        QtCore.QThread.__init__(self, parent)
+        super(MakeCSV, self).__init__()
         self.data = data
         self.filename = filename
 
@@ -1315,7 +1318,8 @@ class MakeCSV(QtCore.QThread):
                  ',' + str(self.data[1][9][x]) + '\n')
         set_download_flag()
         self.daysim_log.info('All data written to CSV file')
-        self.emit(QtCore.SIGNAL('update'))
+        self.update.emit()
+        self.finished.emit()
 
 def main():
     """ PURPOSE: Creates app and runs widget """
