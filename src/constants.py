9e934986545e6d43f1e7ff8678b7ff60f91a180f
plummt <plummt@rpi.edu>
2016-09-29 10:59:48 -0400

    Constants Correction
    Fixes file names for the new constants file names, instead of the
    calibration files.

--- src/constants.py
+++ src/constants.py
@@ -23,14 +23,6 @@ CALIBRATION_FILENAME = '//root/projects/DaysimeterAndDimesimeterReference' \
 #LOCAL_CALIB_FILENAME is the name of the calibration file to be found
 #locally.
 LOCAL_CALIB_FILENAME = 'C:/Daysimeter Client/Day12 RGB Values.txt'
-#NEW_CALIBRATION_FILENAME is the full path to the calibration file stored
-#on the LRC's server. Will be obsolete when calibration is located
-#in the header files. This is the file for daysimeters 366-415
-NEW_CALIBRATION_FILENAME = '//root/projects/DaysimeterAndDimesimeterReference' \
-    + 'Files/data/New Day12 RGB Values.txt'
-#LOCAL_CALIB_FILENAME is the name of the calibration file to be found
-#locally.This is the file for daysimeters 366-415
-NEW_LOCAL_CALIB_FILENAME = 'C:/Daysimeter Client/New Day12 RGB Values.txt'
 #CONSTANTS_FILENAME is the name of the constants stored
 #on the LRC's server. Will be obsolete when calibration is located
 #in the header files.
@@ -39,6 +31,16 @@ CONSTANTS_FILENAME = '//root/projects/DaysimeterAndDimesimeterReference' \
 #LOCAL_CONST_FILENAME is the name of the constants file to be found
 #locally.
 LOCAL_CONST_FILENAME = 'C:/Daysimeter Client/Day12 Cal Values.txt'
+
+#CONSTANTS_FILENAME is the name of the constants stored
+#on the LRC's server. Will be obsolete when calibration is located
+#in the header files.
+NEW_CONSTANTS_FILENAME = '//root/projects/DaysimeterAndDimesimeterReference' \
+    + 'Files/data/New Day12 Cal Values.txt'
+#LOCAL_CONST_FILENAME is the name of the constants file to be found
+#locally.
+NEW_LOCAL_CONST_FILENAME = 'C:/Daysimeter Client/New Day12 Cal Values.txt'
+
 #MINUTES relates to the lowpassfilter.py and is the window the filter
 #uses when smoothing out data.
 MINUTES = 5
